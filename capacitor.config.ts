2e80ef866f638bfd5688e2fc903343dacebfa80f
lmpyprl <125215222+lmpyprl@users.noreply.github.com>
2023-06-30 12:21:46 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobdevCA2',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
